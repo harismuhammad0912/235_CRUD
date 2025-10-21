0e342309f5e141173dcab56a957098119f3d98e3
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-10-21 10:47:14 +0700

    fitur: tambah endpoint /biodata dan ambil data dari MySQL

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(Example app listening on port ${port});
+  console.log(`Example app listening on port ${port}`);
 });
 
 const db = mysql.createConnection({
@@ -19,4 +19,23 @@ const db = mysql.createConnection({
   password: 'haris0912',
   database: 'biodata',
   port: 3306
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the MySQL:', err.stack);
+    return;
+  }
+  console.log('Connection Succesfully!');
+});
+
+app.get('/biodata', (req, res) => {
+  db.query('SELECT * FROM mahasiswa', (err, results) => {
+    if (err) {
+        console.error('Error fetching data:', err.stack);
+        res.status(500).send('Error fetching data');
+        return;
+    }
+    res.json(results);
+  });
 });
